90a898079a1c81929b29ff167d9f0e14f730bf89
sarahtran139@gmail.com <sarahtran139@gmail.com>
2023-07-08 23:37:12 -0400

    assessment3

---
+++ profile.js
@@ -0,0 +1,13 @@
+document.getElementById("color").addEventListener("click", function() {
+    alert("My favorite color is white");
+});
+
+
+document.getElementById("place").addEventListener("click", function() {
+    alert("My favorite place is home");
+});
+
+
+document.getElementById("ritual").addEventListener("click", function() {
+    alert("My favorite ritual is doing yoga when I feel stress");
+});
